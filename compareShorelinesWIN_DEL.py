bfeab0f4fdf24a8043f24f3c472ebc541604daa0
Steven Sholes <sfsholes@uw.edu>
2020-04-21 14:11:16 -0700

    Updated to version used in submission
    Updated this to the version used in the submitted manuscript. Data collection methodology was changed to improve results and most changes reflect the new data parameters. Removed the check on whether a "shoreline" version crosses itself at a given longitude. This may be worth going back to at some point, but does not majorly affect the results (as sections where this is important are removed from the stats in the paper, e.g., where "shorelines" track near-longitudinally, with explanation).

--- compareShorelinesWIN_DEL.py
+++ compareShorelinesWIN_DEL.py
@@ -22,8 +22,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 from geopy import distance
 
-PATH = "D://Research//05_GlobalMarsShorelines//1_Input//CitedShorelines//ZachRemaps//PointsOutput"
+PATH = "D://Research//05_GlobalMarsShorelines//2_Work//IntersectShorelines//ArabiaLevel_CSV"
 TOL = 0.25         # Tolerance for Longitude increments (in degrees)
+#Purging this, fixed it in data collection instead
 
 ## Go through all inputs in the directory to find the range
 LON_LIST = []        # Stores all the longitude values we are checking
@@ -35,24 +36,26 @@ DIFF_LIST = []       # Will store tuples (LAT DIFF, LON)
 # Open the CSV files in the PATH directory
 # Builds THE LON and TUP LISTS for analysis
 file_num = 0        # This labels each shoreline file
-for filename in os.listdir(os.path.join(PATH,"data")):
+for filename in os.listdir(PATH):
     # FINDS ALL TXT AND CSV FILES IN DIRECTORY TO OPEN
     if filename.endswith(".txt") or filename.endswith(".csv"):
         print("OPENING..." + filename)
-        file = open(os.path.join(PATH,"data",filename), newline='')
+        file = open(os.path.join(PATH,filename), newline='')
         reader = csv.reader(file)
         header = next(reader)       # The first line is the header
         
         data = []
         for row in reader:
-            # row = [OBID, FID, NAME, LENGTH, LAT, LON]
+            # row = [OBID, FID, ID, FID, ID, LAT, LON]
             LON = float(row[-1])
             LON_ROUND = round(LON*(1/TOL))/(1/TOL)      #Rounds to nearest TOL
-            # TODO - ABOVE COULD CAUSE MINOR PROBLEMS, BUT SHOULD BE MINIMAL
             LAT = float(row[-2])
             LON_LIST.append(LON_ROUND)
             # GENERATE A LIST OF ALL TOTAL POINTS
-            TUP_LIST.append((LAT, LON_ROUND, file_num))
+            POINT = (LAT, LON_ROUND, file_num)
+            # Removes accidental duplicate points (purge if certain about data collection as slow)
+            if POINT not in TUP_LIST:
+                TUP_LIST.append(POINT)
         file_num += 1
     else:
         continue
@@ -80,13 +83,16 @@ LON_LIST_UNIQ.sort()
 for item in LON_LIST_UNIQ:
     # Make a list of all tuples with the LON we are examining
     TUP_LIST_TEMP = [j for i, j in enumerate(TUP_LIST) if j[1] == item]
-    # i is the enumerate value (0,1,2...), j is the tuple (0=lat, 1=lon_round, 2=file)
+    # i is the enumerate value (0,1,2...), j is the tuple (0=lat, 1=lon, 2=file)
+    print(f"item:  {item}, len: {len(TUP_LIST_TEMP)}")
     
     if len(TUP_LIST_TEMP) == 0:        #Check for empty set
+        print("item does not exist")
         max_lat_tup = Null
         min_lat_tup = Null
         lat_diff = Null
     elif len(TUP_LIST_TEMP) == 1:        #When there is only one point for that longitude
+        print("only one item")
         max_lat_tup = TUP_LIST_TEMP[0]
         min_lat_tup = TUP_LIST_TEMP[0]
         lat_diff = 0
@@ -100,55 +106,65 @@ for item in LON_LIST_UNIQ:
         # HOWEVER, TO FIND MAX LATERAL DIST, WE WANT TO BE CONSERVATIVE
         # AND NOT INCLUDE LEVELS THAT WRAP OVER THEMSELVES TWICE
     
-        # 1) Sort all long points in descending order
-        LIST_PTS = tup_list.sort(reverse=True)
-        # 2) Find how many unique file pts are present
-        unique_pts = len(set(TUP_LIST_TEMP, key=lambda item:item[-1]))
-        # 3) If all unique, just determine dist from OVERALL Min/Max
-        if len(TUP_LIST_TEMP) == unique_pts:
-            lat_diff = distConv(OVERALL_MAX, OVERALL_MIN)
-        # 4) Else, remove duplicates, compute distances, find max
-        else:
-            i = 0
-            TEMP_MAX_DIST = 0
-            while i <= unique_pts:
-                LIST_PTS_TMP = LIST_PTS
-         # 5) Remove the max from the lists, remove duplicates
-                temp_max = LIST_PTS_TMP.pop(0)
-                LIST_PTS.pop(0)
-         # 6) Remove duplicate files, new list where files are different from temp_max file
-                LIST_PTS_TMP = list(filter(lambda x: x[-1] != temp_max[-1], LIST_PTS_TMP))
-         # 7) Calculate max distance
-                list_pts_dist = distConv(temp_max, min(LIST_PTS_TMP, key=lambda item:item[0]))
-         # 8) If greater than previous ones, update it
-                if list_pts_dist > TEMP_MAX_DIST:
-                    TEMP_MAX_DIST = list_pts_dist
-                i += 1
-         # 9) Update lat_diff
-            lat_diff = TEMP_MAX_DIST
+        lat_diff = distConv(OVERALL_MAX, OVERALL_MIN)
+        print(f"   lat_diff: {lat_diff:0.5}")
+        
+        # # 1) Sort all long points in descending order
+        # LIST_PTS = TUP_LIST_TEMP
+        # LIST_PTS.sort(key=lambda item:item[0], reverse=True)
+        # # 2) Find how many unique file pts are present
+        # unique_pts = len(set(item[-1] for item in TUP_LIST_TEMP))
+        # print(unique_pts)
+        # # 3) If all unique, just determine dist from OVERALL Min/Max
+        # if len(TUP_LIST_TEMP) == unique_pts:
+        #     lat_diff = distConv(OVERALL_MAX, OVERALL_MIN)
+        #     print(f"   lat_diff: {lat_diff:0.5}")
+        # # 4) Else, remove duplicates, compute distances, find max
+        # else:
+        #     i = 0
+        #     TEMP_MAX_DIST = 0
+        #     while i < unique_pts:
+        #         print(f"i = {i}")
+        #         LIST_PTS_TMP = LIST_PTS
+        #         print(f"   LIST_PTS: {LIST_PTS}")
+        #  # 5) Remove the max from the lists, remove duplicates
+        #         temp_max = LIST_PTS_TMP.pop(0)
+        #         LIST_PTS.pop(0)
+        #  # 6) Remove duplicate files, new list where files are different from temp_max file
+        #         LIST_PTS_TMP = list(filter(lambda x: x[-1] != temp_max[-1], LIST_PTS_TMP))
+        #         print(f"   LIST_PTS_TMP: {LIST_PTS_TMP}")
+        #  # 7) Calculate max distance
+        #         list_pts_dist = distConv(temp_max, min(LIST_PTS_TMP, key=lambda item:item[0]))
+        #  # 8) If greater than previous ones, update it
+        #         if list_pts_dist > TEMP_MAX_DIST:
+        #             TEMP_MAX_DIST = list_pts_dist
+        #         i += 1
+        #  # 9) Update lat_diff
+        #     lat_diff = TEMP_MAX_DIST
+        #     print(f"   lat_diff: {lat_diff:0.5}")
    
     # Update the min/max values (irrevlevant for max lateral distance)
     MAX_LIST.append(OVERALL_MAX)
     MIN_LIST.append(OVERALL_MIN)
-    DIFF_LIST.append((LAT_DIFF, item))
+    DIFF_LIST.append((lat_diff, item))
     
     ####### -- FOR TESTING -- ######
     if item == -43:
         print(f"-43E MAX: {OVERALL_MAX}, MIN: {OVERALL_MIN}, DIFF: {lat_diff}") 
     
 # Write the new CSV files for importing into ArcMap
-with open(os.path.join(PATH,"output",'max_line.csv'), 'w', newline='') as out:
-    max_out = csv.writer(out)
-    max_out.writerow(["Lat","Lon"])
-    for row in MAX_LIST:
-        max_out.writerow(row)
-        
-with open(os.path.join(PATH,"output",'min_line.csv'), 'w', newline='') as out:
-    min_out = csv.writer(out)
-    min_out.writerow(["Lat","Lon"])
-    for row in MIN_LIST:
-        min_out.writerow(row)
-    
+# with open(os.path.join(PATH,"output",'max_line.csv'), 'w', newline='') as out:
+#     max_out = csv.writer(out)
+#     max_out.writerow(["Lat","Lon"])
+#     for row in MAX_LIST:
+#         max_out.writerow(row)
+#         
+# with open(os.path.join(PATH,"output",'min_line.csv'), 'w', newline='') as out:
+#     min_out = csv.writer(out)
+#     min_out.writerow(["Lat","Lon"])
+#     for row in MIN_LIST:
+#         min_out.writerow(row)
+#     
 with open(os.path.join(PATH, "output",'shore_diff.csv'), 'w', newline='') as out:
     diff_out = csv.writer(out)
     diff_out.writerow(["Diff, Deg","Lon"])
@@ -217,8 +233,6 @@ for row in DEL_FILE:
 
 x_data1 = [x[1] for x in DIFF_LIST]
 y_data1 = [x[0] for x in DIFF_LIST]
-x_data2 = [x[1] for x in DIFF_LIST_WIN]
-y_data2 = [x[0] for x in DIFF_LIST_WIN]
 
 D_COLORS = ['#800000', '#9A6324', '#E6194B', '#F58231', '#FABEBE', '#FFE119']
 A_COLORS = ['#4363D8', '#AAFFC3', '#F032E6', '#000000', '#000075', '#E6BEFF', '#A9A9A9', '#FFFAC8']
@@ -226,7 +240,7 @@ A_COLORS = ['#4363D8', '#AAFFC3', '#F032E6', '#000000', '#000075', '#E6BEFF', '#
 fig, axs = plt.subplots(3,1)
 j = 0
 while j < len(FILE_NAMES_D):
-    axs[0].plot(FILE_LONS_D[j], FILE_ELEVS_D[j], '.', label=str(FILE_NAMES_D[j]), markersize=1, color=D_COLORS[j])
+    axs[0].plot(FILE_LONS_D[j], FILE_ELEVS_D[j], '.', label=str(FILE_NAMES_D[j]), markersize=0.8, color=D_COLORS[j])
     j += 1
 axs[0].plot(DEL_LONS[:], DEL_ELEVS[:], marker="s", markersize=3, mfc="#E9FFBE", mec="k", linewidth=0)
 axs[0].set_xlim(-180,180)
@@ -243,7 +257,7 @@ print("LATS_A:  ", len(FILE_ELEVS_A))
 print("A_COLORS:", len(A_COLORS))
 while j < len(FILE_NAMES_A):
     print(FILE_NAMES_A[j])
-    axs[1].plot(FILE_LONS_A[j], FILE_ELEVS_A[j], '.', label=str(FILE_NAMES_A[j]), markersize=1, color=A_COLORS[j])
+    axs[1].plot(FILE_LONS_A[j], FILE_ELEVS_A[j], '.', label=str(FILE_NAMES_A[j]), markersize=0.8, color=A_COLORS[j])
     j += 1
 axs[1].plot(DEL_LONS[:], DEL_ELEVS[:], marker="s", markersize=3, mfc="#E9FFBE", mec="k", linewidth=0)
 axs[1].set_xlim(-180,180)
@@ -255,6 +269,7 @@ axs[1].grid(which='minor', linestyle=':', linewidth=0.4)
 
 axs[2].plot(x_data1, y_data1, 'k')
 axs[2].set_xlim(-180,180)
+axs[2].set_ylim(0,2600)
 #axs[2].set_xlabel('Longitude [deg E]')
 axs[2].set_ylabel('Max Distance [km]')
 axs[2].minorticks_on()
